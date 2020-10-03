81c2cf86524387e565f3fac28f105828b115d605
simran <simrangoyu@gmail.com>
2020-10-03 20:19:07 +0530

    updated website

--- project.js
+++ project.js
@@ -11,3 +11,37 @@ function func(x){
 }
 function myresult(){
 	window.alert("Your score is " + sum);}
+
+
+
+
+
+	$(document).ready(function() {
+		$("#links").hide();
+		$("#menu-btn").click(changeNav);
+	  
+		function changeNav() {
+		  $("#links").toggle(200);
+		  $("#menu-btn").toggleClass("turn");
+		}
+	  });
+
+
+
+
+
+	  setTimeout(
+		function()
+		{
+			$("#o2").show();
+		},
+		5000
+	)
+	setTimeout(
+		function()
+		{
+			$("#o3").show();
+		},
+		10000
+	)
+	
